1a2fcae7a0ba6e548623c8a2f98f660e11706beb
Gord Brown <gdbzork@gmail.com>
2016-04-10 18:03:57 +0100

    blah

--- R/pilot_utils.R
+++ R/pilot_utils.R
@@ -11,13 +11,15 @@ reads_in_fastq <- function(fns) {
 }
 
 reads_in_single_bam <- function(fn) {
-  pilot_log$info("Counting fastq reads in %s...",fn)
-  cmd <- sprintf("zcat %s | wc -l",fn)
+  pilot_log$info("Counting bam reads in %s...",fn)
+  cmd <- sprintf("samtools flagstat %s",fn)
   s <- system(cmd,intern=TRUE)
-  reads <- as.numeric(s) / 4 # because 4 lines per Fastq read
+  n <- strsplit(s[5]," ")[[1]][1]
+  reads <- as.numeric(n) / 2 # pairs
   return(reads)
 }
 
 reads_in_bam <- function(fns) {
+  pilot_log$info("wd: %s",getwd())
   return(as.numeric(sapply(fns,reads_in_single_bam)))
 }
